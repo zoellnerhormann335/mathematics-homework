02f9db2356dd83b8ae6599e23c5c7d5056be4a6f
zoellnerhormann335 <zoellnerhormann335@gmail.com>
2025-03-16 09:20:49 +0000

    Add 11.c

---
+++ src/11.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int x = 5;
+    float y = 3.14;
+    char z = 'A';
+    printf("The value of x is %d\n", x);
+    printf("The value of y is %f\n", y);
+    printf("The value of z is %c\n", z);
+    return 0;
+}
