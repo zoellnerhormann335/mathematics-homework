5a167026e78d16dca1cb8799529cc06382dcab38
zoellnerhormann335 <zoellnerhormann335@gmail.com>
2025-03-19 04:34:54 +0000

    Add 13.c

---
+++ src/13.c
@@ -0,0 +1,11 @@
+
+// This is a comment in C programming language
+#include <stdio.h>
+
+int main() {
+  int x = 10;
+  printf("x = %d\n", x);
+  return 0;
+}
+
+This code will print the value of variable "x" which is 10 to the screen.
