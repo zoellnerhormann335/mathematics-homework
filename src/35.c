ff530f79d12f7fe96ee5fa388b271b322fb2e788
zoellnerhormann335 <zoellnerhormann335@gmail.com>
2025-04-27 01:49:14 +0000

    Add 35.c

---
+++ src/35.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int num1 = 5;
+    int num2 = 3;
+
+    if (num1 > num2) {
+        printf("Num1 is greater than Num2.\n");
+    } else {
+        printf("Num1 is not greater than Num2.\n");
+    }
+
+    return 0;
+}
